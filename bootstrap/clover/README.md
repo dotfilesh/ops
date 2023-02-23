fcaaf2366b85e8bde8b5327aae9061464a19eb61
Whit Huntley <rwx@whits.io>
2023-02-23 09:39:30 -0500

    docs(init): Begin clover docs.
    Additional info will be needed from unpublished blog, uploaded to
    ProtonDrive.
    Also added a random script

--- bootstrap/clover/README.md
+++ bootstrap/clover/README.md
@@ -1,5 +1,16 @@
 # Clover
 
-Clover is a bootloader.
+> Convey: 
+>
+> 1. Clover was a fix to a "solve once" problem.
+> 2. Get version of clover used. Docs provided are known to be functional only with said version. Any API changes since then are not accounted for.
 
-NVME.
+[Clover](https://github.com/CloverHackyColor/CloverBootloader) is a bootloader originally developed for the purpose of tricking machines into thinking they're macintoshes. It comes with a variety of EFI drivers to preload modules which macOS expects to be provided by the T2 security chip and iBoot (or however Apple Silicon works now-a-days).
+
+Clover is used in this project to provide NVMe boot support to hardware which otherwise does not provide it, such as the 13th generation Dell PowerEdge servers originally used in kclt-01.
+
+NVMe disks are used as the primary storage device for talos due to the necessary disk performance for the ephemeral data (i.e. etcd). 
+
+# Summary Config
+
+1. Clover provides its own NVMe driver, `NvmExpressDxe.efi`, this was enabled 
--- bootstrap/scripts/bootstrap.sh
+++ bootstrap/scripts/bootstrap.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-
 set -o nounset
 set -o errexit
 
 # TODO: set working directory to root of repo
 # TODO: set kubeconfig as a variable, do not assume it is kctl --whatever
 
-kubectl --kubeconfig ${kubeconfig} apply -k k8s/global/init/bootstrap/
-echo "Please add this public key to the ops repository deploy keys"
+# Initial setup, installs bare necessities (in this case flux)
+kubectl --kubeconfig ${kubeconfig} apply -k k8s/init/bootstrap/
+
+# I mean, creates a secret, that much is obvious.
+# However I am not sure if this + the below kubectl command do the
+# equivalent of `flux bootstrap` or if that needs to be done first.
+printf "Please add this public key to the ops repository deploy keys\n"
 flux --kubeconfig ${kubeconfig} create secret git github-token \
   --url=ssh://git@github.com/dotfilesh/ops \
   --ssh-key-algorithm=ed25519
-kubectl --kubeconfig ${kubeconfig} apply --kustomize k8s/clusters${site_id}/flux/flux-system/
-flux --kubeconfig ${kubeconfig} reconcile source git flux-cluster
+# this command originally had a spelling error, which leads me to
+# believe this whole script was just a set of dummy values.
+kubectl --kubeconfig ${kubeconfig} apply --kustomize k8s/clusters/${site_id}/flux/flux-system/
+
+# by this point, regardless of above, flux definately is set up.
+# so it reconsiles everything from the ops repo and applies it
+flux --kubeconfig ${kubeconfig} reconcile source git ops
