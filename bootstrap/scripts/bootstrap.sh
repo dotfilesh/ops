7decb22ffd57266c6da55b3322426f1b5ae2b6d1
Whit Huntley <rwx@whits.io>
2023-08-18 20:26:09 -0400

    i will get this going if it kills me.

--- bootstrap/scripts/bootstrap.sh
+++ bootstrap/scripts/bootstrap.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-##########################################
-# DO NOT USE. NOT DONE. FIX. GODDAMN IT. #
-##########################################
+# This is kinda ugly as it has to handle sensitive information.
+# that is why the umask and log is set up like it is.
+
+
+# This script will bootstrap FluxCD on a Talos cluster.
+# running talos.sh is a prerequisite to this. If you have not yet
+# prepared the machines: DO THAT FIRST!!!
+
+# This script assumes the following:
+# - age
+# - flux
+# - kubectl
+# - sops
+# if you do not have these installed, or do not have the necessary
+# decryption keys for the SOPS encrypted files: DO THAT FIRST!!!
+
+
+
 
 # explicit PATH, and umask
 export PATH=/usr/bin:/bin/:/usr/sbin:/sbin
-umask 2
+umask 0077
 
 # Start logging things, put both STDERR and STDOUT into said log.
-ME=$(basename "$0")
-exec 3>&2 >> /opt/log/${ME}.log.nocommit 2>&1
+ME=$(basename "${0}")
+ABS_DIR=$(dirname "$(realpath "${0}")")
+exec 3>&2 >> "${ABS_DIR}"/"${ME}"-$(date +%s).log.nocommit 2>&1
 # sets:
 # - errexit: exit if simple command fails (nonzero return value)
 # - nounset: write message to stderr if trying to expand unset variable
@@ -20,29 +36,52 @@ set -euvx
 uname -a
 date
 
-# TODO: make sure script runs in root of repo as working dir.
+failure () { echo FAILED. >&3; exit 1; }
+trap failure EXIT
+
+# move to root of repo. This does assume the script is "somewhere" in
+# the repo. Which I think is a perfectly healthy assumption to make.
+cd "${ABS_DIR}"
+cd "$(git rev-parse --show-toplevel)"
 
 # Install flux
-kubectl --kubeconfig ${kubeconfig} apply --server-side --kustomize ./k8s/global/bootstrap/
+kubectl --kubeconfig ${kubeconfig} apply --server-side --kustomize \
+  ./k8s/global/bootstrap/
 
-# Compile agekey file using site_id + global key into secret and apply.
-# TODO: figure out how to make this happen securely.
-sops --config <(echo '') --decrypt ./k8s/clusters/${site_id}/bootstrap/site.agekey.dirtysops >> #???
-sops --config <(echo '') --decrypt ./k8s/global/bootstrap/global.agekey.dirtysops >> # ???
+# This decrypts the sops-encrypted global and site age keys, 
+# concatenates them through an array (TODO: there's gotta be a better
+# way), and creates a kubernetes secret with the data in a format
+# expected by flux.
+# Special thanks to Kay for helping me with this.
+{ sops --config <(echo '') --decrypt \
+  ./k8s/clusters/${cluster_name}/bootstrap/site.agekey.dirtysops ; \
+sops --config <(echo '') --decrypt \ 
+  ./k8s/global/bootstrap/global.agekey.dirtysops } |
 kubectl create secret generic sops-age \
   --namespace=flux-system \
-  --from-file=age.agekey= # ???
+  --from-file=age.agekey=/dev/stdin
 
 
 # Decrypt and apply GitHub deploy key.
-sops  --decrept ./k8s/clusters/${site_id}/bootstrap/github-deploy-key.sops.yaml | kubectl apply -f -
+sops --decrept \
+  ./k8s/clusters/${cluster_name}/bootstrap/github-deploy-key.sops.yaml\
+  | kubectl apply -f -
 
 # Apply values for flux variable substitution
 kubectl apply -f ./k8s/global/config/global-config.yaml
-kubectl apply -f ./k8s/clusters/${site_id}/flux/vars/cluser-config.yaml
-sops --decrypt ../k8s/global/config/global-secrets.sops.yaml | kubectl apply -f -
-sops --decrypt ./k8s/clusters/${site_id}/flux/vars/cluster-secrets.sops.yaml | kubectl apply -f -
+kubectl apply -f \
+  ./k8s/clusters/${cluster_name}/flux/vars/cluser-config.yaml
+sops --decrypt ../k8s/global/config/global-secrets.sops.yaml |\
+  kubectl apply -f -
+sops --decrypt \
+  ./k8s/clusters/${cluster_name}/flux/vars/cluster-secrets.sops.yaml |\
+  kubectl apply -f -
   
-# this command originally had a spelling error, which leads me to
-# believe this whole script was just a set of dummy values.
-kubectl --kubeconfig ${kubeconfig} apply --kustomize k8s/clusters/${site_id}/flux/config/
+# Begin reconciling flux repository.
+kubectl --kubeconfig ${kubeconfig} apply --kustomize \
+  k8s/clusters/${cluster_name}/flux/config/
+
+echo "Please delete the log file, as it contains HIGHLY SENSITIVE INFORMATION."
+  
+# end of script. Therefore the only time failure should be untrapped.
+trap - EXIT
